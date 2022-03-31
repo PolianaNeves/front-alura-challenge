8ebce5061562c635cca50768db8525616b4d3a0a
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-03-31 19:55:54 -0300

    add export as image function

--- src/components/CodeBox/CodeBox.js
+++ src/components/CodeBox/CodeBox.js
@@ -2,19 +2,29 @@ import { useState } from "react";
 import ProjectCard from "./ProjectCard/ProjectCard";
 import ProjectSideMenu from "../ProjectSideMenu/ProjectSideMenu";
 import "./CodeBox.css";
+import { exportAsImage } from "../../helpers/export-images";
+import { useRef } from "react/cjs/react.development";
 
 export default function CodeBox(props) {
     const [language, setLanguage] = useState("");
     const [highlight, setHighlight] = useState(false);
+    const exportProjectCard = useRef(null);
 
     const getSelectedLanguage = (selectedLanguage) => {
-        setLanguage(selectedLanguage)
-    }
+        setLanguage(selectedLanguage);
+    };
 
     return (
         <section className={"codebox-section"}>
             <div className={"code-editor-section"}>
-                <ProjectCard readOnly={false} project={props.project} highlight={highlight} language={language}/>
+                <div ref={exportProjectCard}>
+                    <ProjectCard
+                        readOnly={false}
+                        project={props.project}
+                        highlight={highlight}
+                        language={language}
+                    />
+                </div>
                 <button
                     type="button"
                     data-testid="highlight-btn"
@@ -25,9 +35,21 @@ export default function CodeBox(props) {
                 >
                     Visualizar com o highlight
                 </button>
+                <button
+                    type="button"
+                    className="button-outlined button-outlined-font"
+                    onClick={() =>
+                        exportAsImage(exportProjectCard.current, "test")
+                    }
+                >
+                    Export as image
+                </button>
             </div>
             <div className="side-menu-section">
-                <ProjectSideMenu project={props.project} onLanguageChange={getSelectedLanguage}/>
+                <ProjectSideMenu
+                    project={props.project}
+                    onLanguageChange={getSelectedLanguage}
+                />
             </div>
         </section>
     );
